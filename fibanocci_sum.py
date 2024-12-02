8c923b6a8e4cb37def12b526920d4d12ee9dd435
Emmanuel Lafenwa <leoemaxie@gmail.com>
2024-12-02 23:53:20 +0100

    bug fix

--- fibanocci_sum.py
+++ fibanocci_sum.py
@@ -1,13 +1,12 @@
-def sum_fibanocci_sequence():
-    """Finds the sum of the first 50 fibanocci sequence"""
+def sum_fibonacci_sequence():
+    """Finds the sum of the first 50 Fibonacci sequence"""
     a, b, sum = 0, 1, 0
 
     for i in range(50):
         sum += a
-        a = b
-        b = a + b
+        a, b = b, a + b
 
     return sum
 
 if __name__ == '__main__':
-    print(sum_fibanocci_sequence())
+    print(sum_fibonacci_sequence())
